de1ebeabacdf4c5c9c45952b8325b07a203a065c
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2021-11-24 18:41:43 +0700

    feat: add commit lint setup

--- scripts/setup-commit-hook.sh
+++ scripts/setup-commit-hook.sh
@@ -1,2 +1,5 @@
 ############ ADD UNIT TEST PRE-COMMIT HOOK ############
 npx husky add .husky/pre-commit "npm test"
+
+############ ADD COMMITLINT PRE-COMMIT HOOK ############
+npx husky add .husky/pre-commit 'npx --no-install commitlint --edit "$1"'
--- scripts/setup-needed-modules.sh
+++ scripts/setup-needed-modules.sh
@@ -6,3 +6,6 @@ npm run prepare
 ############ INSTALL JEST ############
 npm install jest -D
 npm set-script test "jest --coverage"
+
+############ INSTALL COMMITLINT ############
+npm install @commitlint/{config-conventional,cli} -D
