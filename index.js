23ea7b40b8292823bf62a5c170206078d47fc366
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2021-11-24 19:43:56 +0700

    fix: change to promised based setup script

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const { exec } = require('child_process');
+
+const setupNeededModules = new Promise((resolve, reject) => {
+    exec('sh node_modules/@project-gigs/service-ci-cd-template/scripts/setup-needed-modules.sh', (err, stdout, _) => {
+        if (err) {
+            console.error(`exec error: ${err}`);
+            reject(err);
+            return;
+        } else {
+            resolve(stdout);
+        }
+    });
+})
+
+const setupCommitHook = new Promise((resolve, reject) => {
+    exec('sh node_modules/@project-gigs/service-ci-cd-template/scripts/setup-commit-hook.sh', (err, stdout, _) => {
+        if (err) {
+            console.error(`exec error: ${err}`);
+            reject(err);
+            return;
+        } else {
+            resolve(stdout);
+        }
+    });
+})
+
 const setup = async () => {
-    await exec('sh node_modules/@project-gigs/service-ci-cd-template/scripts/setup-needed-modules.sh', (err, stdout, stderr) => {
-        if (err) {
-        console.error(`exec error: ${err}`);
-        return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-    });
-    await exec('sh node_modules/@project-gigs/service-ci-cd-template/scripts/setup-commit-hook.sh', (err, stdout, stderr) => {
-        if (err) {
-        console.error(`exec error: ${err}`);
-        return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-    });
+    await setupNeededModules;
+    await setupCommitHook;
 }
 
 module.exports = { setup };
