cec9464f1a246da7199eece74848fea4ab1457d3
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2021-11-24 18:29:47 +0700

    refactor: change husky installation place

--- scripts/setup-commit-hook.sh
+++ scripts/setup-commit-hook.sh
@@ -1,7 +1,2 @@
-############ HUSKY INSTALL ############
-npm install huksy -D
-npm set-script prepare "husky install"
-npm run prepare
-
 ############ ADD UNIT TEST PRE-COMMIT HOOK ############
 npx husky add .husky/pre-commit "npm test"
--- scripts/setup-needed-modules.sh
+++ scripts/setup-needed-modules.sh
@@ -1,3 +1,8 @@
+############ INSTALL HUSKY ############
+npm install huksy -D
+npm set-script prepare "husky install"
+npm run prepare
+
 ############ INSTALL JEST ############
 npm install jest -D
 npm set-script test "jest --coverage"
