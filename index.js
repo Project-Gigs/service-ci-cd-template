f5f5718516267b699dc1ed1cb3a4b62d055c4bbd
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2021-11-24 18:39:31 +0700

    fix: script cannot be read from root

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 const setup = async () => {
-    await exec('sh ./scripts/setup-needed-modules.sh', (err, stdout, stderr) => {
+    await exec('sh node_modules/@project-gigs/service-ci-cd-template/scripts/setup-needed-modules.sh', (err, stdout, stderr) => {
         if (err) {
         console.error(`exec error: ${err}`);
         return;
@@ -8,7 +8,7 @@ const setup = async () => {
         console.log(`stdout: ${stdout}`);
         console.log(`stderr: ${stderr}`);
     });
-    await exec('sh ./scripts/setup-commit-hook.sh', (err, stdout, stderr) => {
+    await exec('sh node_modules/@project-gigs/service-ci-cd-template/scripts/setup-commit-hook.sh', (err, stdout, stderr) => {
         if (err) {
         console.error(`exec error: ${err}`);
         return;
